8fc143d28568fa3bf524dbffd45cf98b5cc51893
kstecyk <123767003+kstecyk@users.noreply.github.com>
2023-10-31 12:07:50 +0100

    Add files via upload

---
+++ Lab1Zad6.cs
@@ -0,0 +1,11 @@
+﻿while (true)
+{
+    Console.Write("Podaj liczbę całkowitą (liczba ujemna kończy program): ");
+    int liczba = Convert.ToInt32(Console.ReadLine());
+
+    if (liczba < 0)
+    {
+        Console.WriteLine("Wprowadzono liczbę mniejszą od zera. Koniec programu.");
+        break;
+    }
+}
